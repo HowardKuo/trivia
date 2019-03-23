e788a35083229b6eba98344be7de58c033d33200
HowardKuo <howardkuo91@gmail.com>
2019-03-23 12:33:18 -0700

    debugging part 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
 var correctScore = 0,
     incorrectScore = 0,
     countdownTimer,
+    timerTimeout,
+    currentIndex = 0,
+    timeoutMs = 5000,
+    secondsLeft = timeoutMs/1000,
     questionBank = [
         new Question('How many NBA teams are based in California?', '4', ['1', '2', '3', '5']),
         new Question('Which player has the most rings?', 'Bill Russell', ['Michael Jordan', 'Robert Horry', 'Sam Jones', 'Larry Bird']),
@@ -9,41 +13,122 @@ var correctScore = 0,
         new Question('Who is the player in the NBA logo?', 'Jerry West', ['Michael Jordan', 'Hakeem Olajuwon', 'Magic Johnson', 'Julius Erving']),
         new Question('How many NBA teams are based outside the United States?', '1', ['0', '2', '3','5']),
         new Question('Which of these players has won a championship?', 'Kevin Garnett', ['Karl Malone', 'John Stockton','Reggie Miller', 'Charles Barkley']),
-        new Question("Which player has won a championship in each of the last 3 decades? (1990's, 2000's,2010's)", 'Tim Duncan', ['Kobe Bryant', "Shaquille O' Neal", 'Dwyane Wade', 'Vince Carter']),
-        new Question('What draft pick was Michael Jordan selected at', '3rd', ['1st', '2nd', '4th', '5th']),
+        new Question("Which player has won a championship in each of the last 3 decades? (1990's, 2000's, 2010's)", 'Tim Duncan', ['Kobe Bryant', "Shaquille O' Neal", 'Dwyane Wade', 'Vince Carter']),
+        new Question('What draft pick was Michael Jordan selected at?', '3rd', ['1st', '2nd', '4th', '5th']),
         new Question('What year did the Seattle Supersonics become the Oklahoma City Thunder?', '2008', ['2010', '2006', '2012', '2004'])
     ];
 
 $(document).ready(function() {
-    console.log(questionBank);
     startGame();
 });
 
-function startGame(){
-    //pull a question from bank
-    //have timer count from 15 to 0
-    //during timer, 4 answers should appear and be clickable
-    //after each question, have a 5 second timer before next question showing right and wrong asnwers
-    //move onto a different random question after time is up
-    shuffle(questionBank)
-    for (var i = 0; i < questionBank.length; i++) {
-        var currentQuestion = questionBank[i];
-        $('.questionAsked').append('<h2>'+ currentQuestion.question + '</h2>');
-        var answers = currentQuestion.incorrectAnswers.slice().concat(currentQuestion.correctAnswer);
-        shuffle(answers);
-        console.log(answers);
-        for (var j = 0; j < answers.length; j++) {
-            $('.answerOptions').append('<div class="answer"><h3>' + answers[j] + '</h3></div><br>');;
+function startGame() {
+    shuffle(questionBank);
+    displayQuestion(currentIndex);
+    countdownTimer = setTimeout(intervalFunction, timeoutMs);
+    $(document.body).on('click','.answer', bindAnswer);
+}
+
+function intervalFunction() {
+    displayCorrectAnswer();
+    //setTimeout(function() {
+        if (currentIndex < questionBank.length - 1) {
+            displayQuestion(++currentIndex);
+            countdownTimer = setTimeout(intervalFunction, timeoutMs);
+            //$('.answer').on('click', bindAnswer);
+            incorrectScore++;
+            displayScores();
         }
-        var test = setTimeout(function(){
-            console.log("SPACEEEEEEEEEEEEEEEEEEEEEEEE")
-        },1000)
-        $('.answer').on('click', function(){
-            clearTimeout(test);
-        });
+        else {
+            clearTimeout(countdownTimer);
+            secondsLeft = 0;
+            $('.questionAsked').html('Click here to try again');
+            displayScores();
+        }
+    // }, 3000);
+}
 
+function displayCorrectAnswer() {
+    $('.answer').each(function(i, elem) {
+        if (questionBank[currentIndex].incorrectAnswers.indexOf($(elem).text()) > -1) {
+            $(elem).addClass('wrongBG');
+        }
+        else {
+            $(elem).addClass('correctBG');
+        }
+    });
+} 
 
+function bindAnswer() {
+    $('.answer').off()
+    displayCorrectAnswer();
+    clearTimeout(countdownTimer);
+    clearInterval(timerTimeout);
+    console.log("right now", questionBank[currentIndex].incorrectAnswers)
+    if ($(this).text() === questionBank[currentIndex].correctAnswer) {
+        correctScore++;
+        console.log("click handler: ", correctScore)
+        if (currentIndex < questionBank.length - 1) {
+            displayQuestion(++currentIndex);
+            countdownTimer = setTimeout(intervalFunction, timeoutMs);
+            //$('.answer').on('click', bindAnswer);
+            
+            displayScores();
+        }
+        else {
+            clearTimeout(countdownTimer);
+            secondsLeft = 0;
+            $('.questionAsked').html('Click here to try again');
+            displayScores();
+        }
     }
+    else {
+        incorrectScore++;
+        console.log("click handler: ", incorrectScore)
+        if (currentIndex < questionBank.length - 1) {
+            displayQuestion(++currentIndex);
+            countdownTimer = setTimeout(intervalFunction, timeoutMs);
+            //$('.answer').on('click', bindAnswer);
+            
+            displayScores();
+        }
+        else {
+            clearTimeout(countdownTimer);
+            secondsLeft = 0;
+            $('.questionAsked').html('Click here to try again');
+            displayScores();
+        }
+    }
+    displayScores();
+    console.log("test")
+    secondsLeft = timeoutMs / 1000;
+    //countdownTimer = setTimeout(intervalFunction, timeoutMs);
+}
+
+function displayQuestion(i) {
+    var current = questionBank[i],
+        answers = current.incorrectAnswers.slice().concat(current.correctAnswer);
+    $('.questionAsked').html('<h2>'+ current.question + '</h2>')
+    shuffle(answers);
+    $('.answerOptions').html('');
+    for (var j = 0; j < answers.length; j++) {
+        $('.answerOptions').append('<div class="answer col-md-12"><h3>' + answers[j] + '</h3></div>');
+    }
+    $('.timer').html('<h4>Time Remaining: ' + secondsLeft + '</h4>')
+    timerTimeout = setInterval(timerFunction, 1000);
+}
+
+function timerFunction() {
+    $('.timer').html('<h4>Time Remaining: ' + --secondsLeft + '</h4>');
+    if (secondsLeft === 0) {
+        clearInterval(timerTimeout);
+        secondsLeft = timeoutMs / 1000;
+    }
+}
+
+function displayScores() {
+    $('.questionsCorrect').text(correctScore);
+    $('.questionsWrong').text(incorrectScore);
     
 }
 
